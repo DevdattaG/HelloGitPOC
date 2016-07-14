e87bd2bc7722bfb29e696847f7157bf0ef395cdb
gogu1986 <gogu1986@gmail.com>
2016-07-14 15:11:40 +0530

    commit from the team

---
+++ HelloGitPOC/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloGitPOC
+{
+    class Class1
+    {
+
+        public Class1()
+        {
+        }
+
+        public int sum() {
+            return 1;
+        }
+        
+    }
+}

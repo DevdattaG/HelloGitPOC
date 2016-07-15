552a03dba60cb97159583e3904ad445349d28779
Devdatta <devdatta.godbole777@gmail.com>
2016-07-15 11:34:35 +0530

    from branching

---
+++ HelloGitPOC/Class4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloGitPOC
+{
+    class Class4
+    {
+        public Class4()
+        {
+            Console.WriteLine("This is from another branch");
+        }
+    }
+}
